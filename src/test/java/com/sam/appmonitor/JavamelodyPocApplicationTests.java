fa42c435031b58da28b12f7e2c56350f06458405
samckp <sam.mca.liet@gmail.com>
2020-01-08 22:38:28 +0530

    Initial code base setup.

---
+++ src/test/java/com/sam/appmonitor/JavamelodyPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.appmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamelodyPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
